f0e4b7c3ad165cf88970f8d839d03fac950885f8
Yahyoxon  Rahimov <78527776+yahyoxonqwe@users.noreply.github.com>
2023-04-18 16:54:59 +0500

    # update

--- streamlit.py
+++ streamlit.py
@@ -7,6 +7,19 @@ st.title('Standford_online_products classification')
 
 ### model ###
 
+classes = [    "bicycle",
+    "cabinet",
+    "chair",
+    "coffee_maker",
+    "fan",
+    "kettle",
+    "lamp",
+    "mug",
+    "sofa",
+    "stapler",
+    "table",
+    "toaster" ]
+
 deep = tf.saved_model.load('model')
 st.header(":blue[Using model]")
 img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
@@ -21,5 +34,6 @@ if st.button('Predict'):
     img_array = cv2.resize(img_array, (224, 224)).astype(np.float32) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
     val = deep(img_array)
-    st.write(f'result: {np.argmax(val[0])}')
+    st.write(f'result : {np.argmax(val[0])}')
+    st.write(f'class  : {classes[np.argmax(val[0])]}')
     st.bar_chart(val[0])
